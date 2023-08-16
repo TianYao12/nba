f9ef88817a69d29729faa206d2251520dcfe5081
TianYao12 <110309420+TianYao12@users.noreply.github.com>
2023-08-16 13:01:00 -0400

    Connected new data to website

--- nba-app/src/pages/api/players/playerapi.js
+++ nba-app/src/pages/api/players/playerapi.js
@@ -1,21 +1,17 @@
 import clientPromise from "../../../../lib/mongodb";
-import axios from "axios";
 
 export default async function handler(req, res) {
   try {
-    const { player } = req.query; // Get the dynamic team name from the query parameters
-    const [first_name, last_name] = player.split(" ");
+    const { player } = req.query;
     const mongoClient = await clientPromise;
-    // Split the player string into first name and last name
     const players = await mongoClient
       .db("nba")
-      .collection("players")
+      .collection("players2")
       .find({
-        first_name: first_name,
-        last_name: last_name,
+        PLAYER: player,
       })
       .toArray();
-
+    console.log("Fetched Players:", players); // Log the fetched players
     res.status(200).json(players);
   } catch (error) {
     console.error(error);
--- nba-app/src/pages/players/index.tsx
+++ nba-app/src/pages/players/index.tsx
@@ -1,58 +1,41 @@
+// React Component
 import axios from "axios";
 import { useEffect, useState } from "react";
 import styles from "../../styles/players.module.css";
 
 interface Player {
-  id: number;
-  first_name: string;
-  last_name: string;
+  _id: string; 
+  PLAYER: string;
+  TEAM: string; 
 }
 
 export default function PlayersPage() {
   const [searchQuery, setSearchQuery] = useState("");
   const [playerData, setPlayerData] = useState<Player[] | null>(null);
-  const [stats, setStats] = useState<any>(null);
-
-  useEffect(() => {
-    if (playerData && playerData.length > 0) {
-      fetchStats(playerData[0].id);
-    }
-  }, [playerData]);
 
   const fetchPlayers = async () => {
     try {
       const response = await axios.get<Player[]>(
-        `/api/players/playerapi?player=${searchQuery}`
+        `/api/players/playerzapi?player=${searchQuery}`
       );
       setPlayerData(response.data);
+      console.log(searchQuery)
     } catch (error) {
       console.error(error);
     }
   };
 
-  const fetchStats = async (id: number) => {
-    try {
-      const response = await axios.get<any>(
-        `https://www.balldontlie.io/api/v1/season_averages?player_ids[]=${id}`
-      );
-      setStats(response.data.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  console.log(playerData);
   return (
     <>
       <div className={styles.search}>
-        <h2>Search for NBA Player</h2>
+        <h2>Search for NBA Player (2012-2013 to 2021-2022 Season)</h2>
         <input
           type="text"
           value={searchQuery}
           onChange={(e) => {
             setSearchQuery(e.target.value);
           }}
-          placeholder="Search for a player by first name"
+          placeholder="Search for a player by full name"
           className={styles.searchBar}
         />
         <button onClick={fetchPlayers} className={styles.button}>
@@ -60,40 +43,17 @@ export default function PlayersPage() {
         </button>
       </div>
 
-      {playerData && stats ? (
+      {playerData ? (
         <ul>
-          <li>
-            <strong>ID:</strong> {playerData[0].id}
-          </li>
-          <li>
-            <strong>First Name:</strong> {playerData[0].first_name}
-          </li>
-          <li>
-            <strong>Last Name:</strong> {playerData[0].last_name}
-          </li>
-          <li>
-            <strong>Games Played:</strong>{" "}
-            {stats.games_played === null ? stats.games_played : "No Data"}
-          </li>
-          <li>
-            <strong>pts:</strong>{" "}
-            {stats.pts === null ? stats.pts : "No Data"}
-          </li>
-        </ul>
-      ) : null}
-
-      {/* This also works:
-       {playerData ? (
-        <ul>
-          {Object.entries(playerData).map(([key, value]) => (
-            <li key={key}>
-              <strong>{key}:</strong> {value}
+          {playerData.map((player) => (
+            <li key={player._id}>
+              <strong>Name:</strong> {player.PLAYER} 
+              <br />
+              <strong>Team:</strong> {player.TEAM} 
             </li>
           ))}
         </ul>
-      ) : (
-        <h1>Loading players...</h1>
-      )} */}
+      ) : null}
     </>
   );
 }
